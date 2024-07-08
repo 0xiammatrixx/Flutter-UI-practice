e96581afe9e85d89422a2f74c4f0bf09dbec34c6
0xiammatrixx <davidotom50@gmail.com>
2024-07-08 00:05:59 +0000

    Jul 8, 2024, 1:05 AM

--- new project/mynotes/lib/views/alertdialogue.dart
+++ new project/mynotes/lib/views/alertdialogue.dart
@@ -27,7 +27,7 @@ Future<void> _showMyDialog (BuildContext context)async {
       content: const SingleChildScrollView(
         child: ListBody(
           children: [
-            Text('Alert Name'),
+            Text('Alert'),
             Text('Alert Description'),  
           ],
         ),
